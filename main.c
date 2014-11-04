27183a682dee61342a70993c9e9ccd9ae3c0de9b
Doug Gaff <doug.gaff@gmail.com>
2014-11-03 20:30:32 -0500

    more

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
   printf("something ");
   printf("needs to be done ");
   printf("about the goose poop.\n");
-  printf("there is not to do.\n")
+  printf("there is nothing to do.\n");
   printf("oh well.\n");
   return 0;
 }
